01667e59b4800c2bd81bf2605e920f1d7599d249
muzhaqisamanta <smuzhaqi@unitsystem.net>
2022-06-09 13:12:34 +0000

    Creating up and down queries for users table

--- hasura-ecomm/migrations/default/1654779864601_users/up.sql
+++ hasura-ecomm/migrations/default/1654779864601_users/up.sql
@@ -1,1 +1,6 @@
-SELECT NOW();
+CREATE TABLE IF NOT EXISTS "public"."users" (
+  "id" TEXT,
+  "name" TEXT NOT NULL,
+  "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+  CONSTRAINT "userss_pkey" PRIMARY KEY ("id")
+)
