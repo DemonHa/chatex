01fa96033fb60093a1ce0cf93379322aeb450bba
DemonHa <hh360284@gmail.com>
2022-06-09 13:24:00 +0000

    Adding rules to sql on update and delete

--- hasura-ecomm/migrations/default/1654779899759_rooms/up.sql
+++ hasura-ecomm/migrations/default/1654779899759_rooms/up.sql
@@ -3,7 +3,12 @@ CREATE TABLE IF NOT EXISTS "public"."rooms" (
   "user_id" TEXT NOT NULL,
   "name" TEXT NOT NULL,
   "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-  CONSTRAINT "rooms_pkey" PRIMARY KEY ("id"),
-  CONSTRAINT "rooms_user_id_fkey"
-  FOREIGN KEY (user_id) REFERENCES "public".users(id)
+  CONSTRAINT "rooms_pkey" PRIMARY KEY ("id")
 );
+
+ALTER TABLE ONLY "public"."rooms"
+ADD CONSTRAINT "rooms_user_id_fkey"
+FOREIGN KEY (user_id) REFERENCES users(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
--- hasura-ecomm/migrations/default/1654779908990_messages/up.sql
+++ hasura-ecomm/migrations/default/1654779908990_messages/up.sql
@@ -4,9 +4,19 @@ CREATE TABLE IF NOT EXISTS "public"."messages" (
   "room_id" TEXT NOT NULL,
   "message" TEXT NOT NULL,
   "created_at" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-  CONSTRAINT "messages_pkey" PRIMARY KEY ("id"),
-  CONSTRAINT "messages_user_id_fkey"
-  FOREIGN KEY (user_id) REFERENCES "public".users(id),
-  CONSTRAINT "messages_room_id_fkey"
-  FOREIGN KEY (room_id) REFERENCES "public".rooms(id)
-)
+  CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+ALTER TABLE ONLY "public"."messages"
+ADD CONSTRAINT "messages_user_id_fkey"
+FOREIGN KEY (user_id) REFERENCES users(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
+
+ALTER TABLE ONLY "public"."messages"
+ADD CONSTRAINT "messages_room_id_fkey"
+FOREIGN KEY (room_id) REFERENCES rooms(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+NOT DEFERRABLE;
